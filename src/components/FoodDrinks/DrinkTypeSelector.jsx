c9691f38f0b2c4dac86081f891703948736cdfdb
araj-abood <alaraj.abdullah02@gmail.com>
2023-09-12 00:10:46 +0300

    Added some extra ui

--- src/components/FoodDrinks/DrinkTypeSelector.jsx
+++ src/components/FoodDrinks/DrinkTypeSelector.jsx
@@ -7,7 +7,7 @@ const DrinkTypeSelector = ({ onDrinkTypeSelect, selectedDrinkType }) => {
       <div className="flex-1 max-lg:hidden flex justify-center items-center">
         <h1 className="text-red-700 text-5xl">Enjoy!!</h1>
       </div>
-      <div className="h-[120px] w-1/2 flex justify-between lg:justify-between ">
+      <div className="h-[120px] border-l-[3px] border-white w-1/2 flex justify-between lg:justify-between ">
         <DrinkTypeCard
           title={"Hot Drinks"}
           img="https://images.unsplash.com/photo-1450024529642-858b23834369?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG90JTIwZHJpbmtzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
